8532861b2308e9be03b84c06eb4ff91e3ddbc5d6
Gustavo André Fontes Lopes <gflopes@MacBook-Pro-de-Gustavo.local>
2020-10-04 21:43:08 -0300

    Ajuste do nome do projeto nos artefatos

--- src/test/java/br/com/gustavo/CountryResourceTest.java
+++ src/test/java/br/com/gustavo/CountryResourceTest.java
@@ -7,7 +7,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class NbaResourceTest {
+public class CountryResourceTest {
 
     @Test
     public void testHelloEndpoint() {
--- src/test/java/br/com/gustavo/NativeCountryResourceIT.java
+++ src/test/java/br/com/gustavo/NativeCountryResourceIT.java
@@ -3,7 +3,7 @@ package br.com.gustavo;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeNbaResourceIT extends NbaResourceTest {
+public class NativeCountryResourceIT extends CountryResourceTest {
 
     // Execute the same tests but in native mode.
 }
